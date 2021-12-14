15533d6e8a8e7a4d8a510a283a9e43ef1dbf29c6
oliwiersalamon <84533631+oliwiersalamon@users.noreply.github.com>
2021-12-14 12:13:30 +0100

    Add files via upload

---
+++ SPULC/pytania.js
@@ -0,0 +1,43 @@
+var pytania = [
+    {
+id: 0,
+question: "Pytanie 1",
+odpowiedzPISEMNA: "nie",
+odpowiedz1: "a1",
+odpowiedz2: "b1",
+odpowiedz3: "c1",
+odpowiedz4: "d1"
+},
+
+{
+    id: 1,
+    question: "Pytanie 2",
+    odpowiedzPISEMNA: "nie",
+    odpowiedz1: "a2",
+    odpowiedz2: "b2",
+    odpowiedz3: "c2",
+    odpowiedz4: "d2"
+    },
+    
+ {   id: 2,
+    question: "Pytanie 3",
+    odpowiedzPISEMNA: "nie",
+    odpowiedz1: "a3",
+    odpowiedz2: "b3",
+    odpowiedz3: "c3",
+    odpowiedz4: "d3"
+},
+    
+{   id: 3,
+   question: "Pytanie 4",
+   odpowiedzPISEMNA: "tak",
+   odpowiedz1: "1234"
+},
+    
+{   id: 4,
+   question: "Pytanie 5",
+   odpowiedzPISEMNA: "tak",
+   odpowiedz1: "12345"
+}
+
+];
